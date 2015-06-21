52d52e9aad2e0897369b5b25d9c5062c44710253
David <david@templatious.org>
2015-06-21 15:40:12 +0300

    domain update

--- Lua-NthPrimeNumberGenerator/domain.h
+++ Lua-NthPrimeNumberGenerator/domain.h
@@ -1,15 +1,22 @@
-#ifndef DOMAIN_H
-#define DOMAIN_H
+#ifndef DOMAIN_8UU5DBQ1
+#define DOMAIN_8UU5DBQ1
 
+#include <map>
+#include <memory>
+#include <mutex>
 #include <lua5.2/lua.hpp>
 
 #include "messeagable.h"
 
 typedef int (*lua_CFunction) (lua_State *L);
 
+namespace templatious {
+    struct DynVPackFactory;
+}
+
 struct LuaContext {
     LuaContext() :
-        _s(luaL_newstate()) {}
+        _s(luaL_newstate()), _fact(nullptr) {}
 
     lua_State* s() {
         return _s;
@@ -22,11 +29,18 @@ struct LuaContext {
     ~LuaContext() {
         lua_close(_s);
     }
+
+    typedef std::shared_ptr< Messageable > StrongMsgPtr;
+    typedef std::weak_ptr< Messageable > WeakMsgPtr;
+
 private:
     lua_State* _s;
+    templatious::DynVPackFactory* _fact;
+    std::mutex _mtx;
+    std::map< const char*, WeakMsgPtr > _messageableMap;
 };
 
 void initDomain(LuaContext& ctx);
 
-#endif // DOMAIN_H
+#endif /* end of include guard: DOMAIN_8UU5DBQ1 */
 
