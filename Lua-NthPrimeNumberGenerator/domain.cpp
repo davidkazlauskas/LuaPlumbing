2deedfa979287ba4ec629bf5db87b0f740b4dac4
David <david@templatious.org>
2015-06-24 17:22:07 +0300

    should werk

--- Lua-NthPrimeNumberGenerator/domain.cpp
+++ Lua-NthPrimeNumberGenerator/domain.cpp
@@ -220,12 +220,13 @@ struct ConstCharTreeNode {
                 valueNode.push(ConstCharTreeNode(
                     keyBuf.c_str(),outVec[i].c_str()));
             } else {
-                typeNode.push(ConstCharTreeNode(keyBuf.c_str(),""))
+                typeNode.push(ConstCharTreeNode(keyBuf.c_str(),""));
                 valueNode.push(ConstCharTreeNode(keyBuf.c_str(),""));
                 auto& tnodeRef = typeNode._children.back();
                 auto& vnodeRef = valueNode._children.back();
                 VPackPtr* vpptr = nullptr;
                 ::memcpy(&vpptr,outVec[i].data(),sizeof(void*));
+                packToTreeRec(tnodeRef,vnodeRef,**vpptr,fact);
             }
         }
     }
