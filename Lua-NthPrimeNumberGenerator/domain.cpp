fa3dde42e20bcd722bfad2c669936b28c36aca55
David <david@templatious.org>
2015-06-30 17:38:11 +0300

    should werk

--- Lua-NthPrimeNumberGenerator/domain.cpp
+++ Lua-NthPrimeNumberGenerator/domain.cpp
@@ -893,6 +893,6 @@ void LuaContext::processSingleMessage(const AsyncMsg& msg) {
     ::lua_getglobal(s(),"PackSnapshot");
     ::lua_setmetatable(s(),-2);
     ::lua_rawgeti(s(),msg->luaTable(),msg->luaFunc());
-    ::lua_pushvalue(s(),-3);
+    ::lua_pushvalue(s(),-2);
     ::lua_pcall(s(),1,0,0);
 }
