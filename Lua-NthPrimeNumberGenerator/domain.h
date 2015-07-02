1fb072953560ff164ae7ab5b67e95c3572e507b4
David <david@templatious.org>
2015-07-02 21:10:19 +0300

    dumbo

--- Lua-NthPrimeNumberGenerator/domain.h
+++ Lua-NthPrimeNumberGenerator/domain.h
@@ -71,10 +71,9 @@ struct VTree {
 
     VTree(const VTree&) = delete;
     VTree(VTree&& other) :
+        _type(other._type),
         _key(std::move(other._key))
     {
-        destructCurrent();
-        _type = other._type;
         switch (other._type) {
             case Type::Double:
                 _double = other._double;
