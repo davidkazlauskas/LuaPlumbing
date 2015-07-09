448a3e7d24d022baf98a7084b6a9baa74f97d4ae
David <david@templatious.org>
2015-07-09 19:18:19 +0300

    make ctx

--- Lua-NthPrimeNumberGenerator/Tests/tests-main.cpp
+++ Lua-NthPrimeNumberGenerator/Tests/tests-main.cpp
@@ -4,6 +4,11 @@
 
 #include "../plumbing.hpp"
 
+std::shared_ptr< LuaContext > getContext() {
+    static auto ctx = LuaContext::makeContext();
+    return ctx;
+}
+
 TEST_CASE("basic_messaging_set","[basic_messaging]") {
 
 }
